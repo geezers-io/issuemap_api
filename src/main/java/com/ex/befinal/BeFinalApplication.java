0ebbf46fc29f6071c77dcfbc0079f671a2dbf3b8
HyukJunJi <whrnjsrkdls@gmail.com>
2023-11-24 17:32:52 +0900

    기본 설정

---
+++ src/main/java/com/ex/befinal/BeFinalApplication.java
@@ -0,0 +1,13 @@
+package com.ex.befinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFinalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BeFinalApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ex/befinal/BeFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.befinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeFinalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
