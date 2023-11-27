1832586d5182aa16d13e59b97ae5db2407e7a051
HyukJunJi <whrnjsrkdls@gmail.com>
2023-11-28 02:21:02 +0900

    querydsl redis 설정

---
+++ src/main/java/com/ex/befinal/global/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package com.ex.befinal.global.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@Configuration
+public class RedisConfig {
+  @Value("${spring.data.redis.host}")
+  private String host;
+
+  @Value("${spring.data.redis.port}")
+  private int port;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(host, port);
+  }
+
+  @Bean
+  public RedisTemplate<?, ?> redisTemplate() {
+    StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+    RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setKeySerializer(stringRedisSerializer);
+    redisTemplate.setHashKeySerializer(stringRedisSerializer);
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+    return redisTemplate;
+  }
+
+}
